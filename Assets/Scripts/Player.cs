cfa653fc847435dfdc810f5ada8c53509cb345df
Dwight Angelo Burbon <dj_burbz_360@yahoo.com>
2019-08-28 23:34:51 +1000

    Code cleanup + UI optimization
    Made gauge bars as child of car game object and UI is set to world space.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -39,79 +39,6 @@ public class Player : MonoBehaviour
     #endregion
 
     #region Methods
-    //Legacy movement
-    /*
-    /// <summary>
-    /// Moves the player only when they have a snack grabbed.
-    /// </summary>
-    private void GoToPointer()
-    {
-        if (hasSnack)
-        {
-            //Move only when the clicked object is an employee and their car is parked.
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D rayHit = Physics2D.Raycast(mousePos, mousePos);
-
-            if (rayHit.transform != null)
-            {
-                if (rayHit.transform.tag == "Employee" &&
-                    rayHit.transform.GetComponent<Employee>().Car.IsParked &&
-                    Input.GetMouseButtonDown(0))
-                {
-                    currentTarget = rayHit.transform.GetComponent<Employee>();
-                    travelPoints[1] = rayHit.transform.position;
-                    isMoving = true;
-                }
-            }
-        }
-    }
-
-    private void MovePlayer()
-    {        
-        if (isMoving)
-        {
-            if (hasSnack)
-            {
-                interpolation += moveSpeed * Time.deltaTime;
-            }
-            else
-            {
-                interpolation -= moveSpeed * Time.deltaTime;
-            }
-        }
-
-        if (interpolation > 1)
-        {
-            interpolation = 1;
-        }
-        else if (interpolation < 0)
-        {
-            interpolation = 0;
-        }
-
-        if (interpolation == 0)
-        {
-            isMoving = false;
-            currentTarget = null;
-        }
-
-        transform.position = Vector2.Lerp(travelPoints[0], travelPoints[1], interpolation);
-    }
-
-    /// <summary>
-    /// Move back to position when delivering the snack is unsuccessful.
-    /// </summary>
-    private void CheckForEmployee()
-    {
-        if (currentTarget != null &&
-            !currentTarget.Car.IsParked)
-        {
-            isMoving = false;
-            interpolation -= moveSpeed * Time.deltaTime;
-        }
-    }
-    */
-
     private void Move()
     {
         if (Input.GetAxis("Horizontal") < 0) //left
@@ -184,10 +111,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //GoToPointer();
-        //MovePlayer();
-        //CheckForEmployee();
-
         if (enableMovement) Move();
         LimitMovement();
     }
--- Assets/Scripts/UI_Initializer.cs
+++ Assets/Scripts/UI_Initializer.cs
@@ -1,52 +1,31 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// Assigns the Respective UI to the Employee prefab, then parent the given UI prefab to the UI tree.
-/// </summary>
 [ExecuteAlways]
 public class UI_Initializer : MonoBehaviour
 {
     [SerializeField] private GameObject timerUI;
-                     private GameObject instanciatedTimer;
-
     [SerializeField] private GameObject fatigueUI;
-                     private GameObject instanciatedFatigue;
-
-    [SerializeField] private Vector2 UIoffset;
-    [SerializeField] private Vector2 UIspacing;
-
-    //void Awake()
-    //{
-    //    if (timerUI != null && gameObject.GetComponent<CarManager>().TimerUI == null)
-    //        instanciatedTimer = Instantiate(timerUI, Vector2.zero, Quaternion.identity, GameObject.FindWithTag("UI").transform);
-
-    //    if (fatigueUI != null && transform.GetChild(0).GetComponent<Employee>().FatigueUI == null)
-    //        instanciatedFatigue = Instantiate(fatigueUI, Vector2.zero, Quaternion.identity, GameObject.FindWithTag("UI").transform);
-    //}
+    [SerializeField] private Vector3 UIoffset;
+    [SerializeField] private Vector3 UIspacing;
 
     private void Awake()
     {
-        if (Application.isPlaying)
-        {
-            if (timerUI != null)
-                gameObject.GetComponent<CarManager>().TimerUI = timerUI;
+        CarManager carManager = this.gameObject.GetComponent<CarManager>();
+        Employee employee = this.gameObject.transform.GetChild(0).GetComponent<Employee>();
 
-            if (fatigueUI != null)
-                transform.GetChild(0).GetComponent<Employee>().FatigueUI = fatigueUI;
-        }
+        if (carManager.TimerUI == null) carManager.TimerUI = timerUI;
+        if (employee.FatigueUI == null) employee.FatigueUI = fatigueUI;
     }
 
     private void Update()
     {
         if (!Application.isPlaying)
         {
-            Vector2 currentScreenPos = this.gameObject.transform.position;
-
             if (timerUI != null)
-                timerUI.transform.position = Camera.main.WorldToScreenPoint(currentScreenPos + UIoffset);
+                timerUI.transform.localPosition = UIoffset;
 
             if (fatigueUI != null)
-                fatigueUI.transform.position = Camera.main.WorldToScreenPoint(currentScreenPos + UIoffset + UIspacing);
+                fatigueUI.transform.localPosition = UIoffset + UIspacing;
         }
     }
 }
