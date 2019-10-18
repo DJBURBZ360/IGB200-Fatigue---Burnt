37dd36eb0d2bc024396c1adb05a1b5d47488dbc7
Dwight Angelo Burbon <dj_burbz_360@yahoo.com>
2019-10-19 02:59:00 +1000

    Added writting SFX for main menu

---
+++ Assets/Scripts/Utilities/SFX_Randomizer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SFX_Randomizer : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] clips;
+    private AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+        int i = Random.Range(0, clips.Length);
+        source.PlayOneShot(clips[i]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!source.isPlaying) Destroy(this.gameObject);
+    }
+}
